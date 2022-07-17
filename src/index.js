123ca0891a85bbf423afd15fe1735a038c484b2c
heyanniemoran <heyanniemoran@gmail.com>
2022-07-17 13:57:27 +0300

    Добавила prettier

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
 export default function useDocumentVisibility() {
   const [count, setCount] = useState(0);
   const [visible, setVisible] = useState(false);
 
   useEffect(() => {
-    document.addEventListener('visibilitychange', function() {
+    document.addEventListener("visibilitychange", function () {
       if (document.visibilityState == "visible") {
         if (!visible) {
-          setCount(count+1);
+          setCount(count + 1);
           setVisible(true);
         }
       } else setVisible(false);
-    })
+    });
   });
 
   function onVisibilityChange(callback) {
-    document.addEventListener('visibilitychange', function() {
+    document.addEventListener("visibilitychange", function () {
       let isVisible = document.visibilityState;
       callback(isVisible);
-    })
+    });
   }
 
   let obj = {
     count: count,
     visible: visible,
-    onVisibilityChange: onVisibilityChange
-  }
-  return (obj)
-};
+    onVisibilityChange: onVisibilityChange,
+  };
+  return obj;
+}
